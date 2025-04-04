a60cfc160e3e3426ea004dfaecb404342d8e424f
Aryan <bhardwaj7a@gmail.com>
2025-04-04 17:26:47 +0530

    Write the Balance component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import { AppBar } from "../components/AppBar.jsx"
+import { Balance } from "../components/BalanceComponent.jsx"
 
 export const DashBoard = () => {
     return <div>
         <AppBar />
+        <Balance />
     </div>
 }
