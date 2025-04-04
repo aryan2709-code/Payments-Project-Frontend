b34d7ac690c5a4623f924526b89601134fcce2e7
Aryan <bhardwaj7a@gmail.com>
2025-04-04 18:15:03 +0530

    Write the sendMoney Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter , Route , Routes } from "react-router-dom"
 import { SignUpPage } from "./pages/SignUpPage.jsx"
 import { SignInPage } from "./pages/SignInPage.jsx"
 import { DashBoard } from "./pages/Dashboard.jsx"
+import { SendMoney } from "./pages/SendMoney.jsx"
 function App() {
 return (
  <BrowserRouter>
@@ -9,6 +10,7 @@ return (
  <Route path="/signup" element={<SignUpPage />} />
  <Route path = "/signin" element = {<SignInPage />} />
  <Route path = "/dashboard" element = {<DashBoard />} />
+ <Route path ="/send" element = {<SendMoney />} />
  </Routes>
  </BrowserRouter>
 )
