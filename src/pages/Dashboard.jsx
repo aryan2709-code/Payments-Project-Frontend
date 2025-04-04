4a224a096eee8fe4a5be8b4ab4f76a7604510a59
Aryan <bhardwaj7a@gmail.com>
2025-04-04 17:50:24 +0530

    Write the UsersComponent

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import { AppBar } from "../components/AppBar.jsx"
 import { Balance } from "../components/BalanceComponent.jsx"
+import { Users } from "../components/UsersComponent.jsx"
 
 export const DashBoard = () => {
     return <div>
         <AppBar />
         <Balance />
+        <Users />
     </div>
 }
