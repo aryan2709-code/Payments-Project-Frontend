cc9195ac85d75c8baebb3dfa878e4611bdb02887
Aryan <bhardwaj7a@gmail.com>
2025-04-04 17:20:23 +0530

    Write the AppBar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter , Route , Routes } from "react-router-dom"
+import { SignUpPage } from "./pages/SignUpPage.jsx"
+import { SignInPage } from "./pages/SignInPage.jsx"
+import { DashBoard } from "./pages/Dashboard.jsx"
 function App() {
 return (
  <BrowserRouter>
  <Routes>
-  <Route path="/signup" element={<SignUp />} />
-  <Route path="/signin" element={<SignIn />} />
-  <Route path="/dashboard" element={<DashBoard />} />
-  <Route path="/send" element={<SendMoney />} />
+ <Route path="/signup" element={<SignUpPage />} />
+ <Route path = "/signin" element = {<SignInPage />} />
+ <Route path = "/dashboard" element = {<DashBoard />} />
  </Routes>
  </BrowserRouter>
 )
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+import { AppBar } from "../components/AppBar.jsx"
+
+export const DashBoard = () => {
+    return <div>
+        <AppBar />
+    </div>
+}
